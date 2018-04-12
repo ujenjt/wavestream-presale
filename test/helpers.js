e4b8213e336646de85dc12176ca26568120051d0
ujenjt <pshenichniy.eugene@gmail.com>
2018-04-12 22:34:27 +0300

    Update happy-path

--- test/helpers.js
+++ test/helpers.js
@@ -1,6 +1,19 @@
+import chai from 'chai'
+export const assert = chai.assert
+
+export async function assertRevert(promise, message) {
+  try {
+    await promise
+    assert.fail('Expected revert not received')
+  } catch (error) {
+    const revertFound = error.message.search('revert') >= 0
+    assert(revertFound, `Expected "revert", got ${error} instead ${message}`)
+  }
+}
+
 export function getAddresses(accounts) {
   const [
-    deployer,
+    owner,
     investor,
     priorityWallet,
     wallet,
@@ -8,11 +21,15 @@ export function getAddresses(accounts) {
     anonymous,
   ] = accounts
   return {
-    deployer,
+    owner,
     investor,
     priorityWallet,
     wallet,
     purchaser,
     anonymous,
   }
+}
+
+export function ether(n) {
+  return new web3.BigNumber(web3.toWei(n, 'ether'))
 }
--- test/test-1-happy-path.js
+++ test/test-1-happy-path.js
@@ -1,7 +1,10 @@
 import BigNumber from 'bignumber.js'
 
 import {
+  assert,
+  assertRevert,
   getAddresses,
+  ether,
 } from './helpers'
 
 const TestToken = artifacts.require('./TestToken.sol')
@@ -10,27 +13,32 @@ const WavestreamPresale = artifacts.require('./WavestreamPresale.sol')
 contract(`WavestreamPresale (happy path):`, accounts => {
   const addr = getAddresses(accounts)
 
-  const rate = 1 // 1 tokens for 1 Ether, assuming token.digits is 18
+  const rate = 100 // 100 tokens for 1 Ether, assuming token.digits is 18
 
   let presale
   let token
 
   before(async () => {
-    token = await TestToken.new({from: addr.deployer})
+    token = await TestToken.new({from: addr.owner})
 
     presale = await WavestreamPresale.new(
       rate,
       addr.priorityWallet,
       '10e18',
       addr.wallet,
-      '10e20',
+      ether(400),
       token.address,
-      {from: addr.deployer}
+      {from: addr.owner}
     )
 
     await token.transfer(presale.address, '100000e18', {
-      from: addr.deployer,
+      from: addr.owner,
     })
+  })
+
+  it(`sets owner to the address which created the contract`, async () => {
+    const owner = await presale.owner()
+    assert.equal(owner, addr.owner)
   })
 
   it('accepts payments', async function () {
@@ -38,5 +46,20 @@ contract(`WavestreamPresale (happy path):`, accounts => {
       value: '10e18',
       from: addr.investor,
     })
+  })
+
+  it(`allows owner to close the crowdsale`, async () => {
+    await presale.closeCrowdsale(
+      {from: addr.owner},
+    )
+  })
+
+  it('it doesnt accepts payments after sale is closed', async function () {
+    await assertRevert(
+      presale.buyTokens(addr.investor, {
+        value: '10e18',
+        from: addr.investor,
+      })
+    )
   })
 })
